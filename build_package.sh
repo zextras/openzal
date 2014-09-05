e4a68dd919c5fcf624e8f317ff018553e0bf9690
Davide Baldo <db@zextras.com>
2014-09-05 13:07:20 +0200

    fixed package building for  zal zextras and zxmig

--- build_package.sh
+++ build_package.sh
@@ -3,15 +3,16 @@
 set -e
 
 VERSION=$(cat version)
+BRIEF_VERSION=$(cat version | sed -E 's/([0-9]\.[0-9])\.[0-9]/\1/')
 
-mkdir -p tmp/
 mkdir -p release/
 rm -rf tmp/*
+mkdir -p tmp/$BRIEF_VERSION/
 
 cd dist/
 for zimbraVersion in *;
 do
-  cp "${zimbraVersion}/zal.jar" "../tmp/${VERSION}-${zimbraVersion}.jar"
+  cp "${zimbraVersion}/zal.jar" "../tmp/$BRIEF_VERSION/zal-${VERSION}-${zimbraVersion}.jar"
 done;
 
 cd ../tmp/
