1be7c5556abfc4627d7837ab14e1ea982174f00c
Davide Baldo <db@zextras.com>
2014-10-03 18:17:29 +0200

    Revert "ZAL minor fix for mailbox in zimbra 7.2.7"
    This reverts commit ca3e9721aec9e8168e73200597c2b940261800fb.

--- src/java/org/openzal/zal/Mailbox.java
+++ src/java/org/openzal/zal/Mailbox.java
@@ -2171,7 +2171,7 @@ public class Mailbox
       Class partypes[] = new Class[1];
       partypes[0] = com.zimbra.cs.mailbox.Mailbox.class;
 
-/* $if ZimbraVersion < 7.2.7 $
+/* $if MajorZimbraVersion < 8 $
       sGetAllFlags = Flag.class.getDeclaredMethod("getAllFlags", partypes);
   $else$ */
       sGetAllFlags = com.zimbra.cs.mailbox.Flag.class.getDeclaredMethod("allOf", partypes);
