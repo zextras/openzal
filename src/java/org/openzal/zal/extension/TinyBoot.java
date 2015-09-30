3de00506d66abf12c5cc006758ab58b06bf8b580
Alessandro Piccioli <ap@zextras.com>
2015-09-30 17:50:06 +0200

    New ZxCLI boot.

--- src/java/org/openzal/zal/extension/TinyBoot.java
+++ src/java/org/openzal/zal/extension/TinyBoot.java
@@ -20,32 +20,25 @@
 
 package org.openzal.zal.extension;
 
-import org.openzal.zal.lib.Version;
+import org.openzal.zal.tools.ExtensionLoader;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 public class TinyBoot
 {
-  private final File mExtensionPathFile;
   private final static String sZalEntrypointName = "org.openzal.zal.extension.ZalEntrypointImpl";
+  private final ExtensionLoader mExtensionLoader;
+  private final File mExtensionPathFile;
 
   public TinyBoot(File extensionPathFile)
   {
     mExtensionPathFile = extensionPathFile;
+    mExtensionLoader = new ExtensionLoader();
   }
 
   public ZalEntrypoint createZalEntryPoint(File extensionDirectory, ZalExtensionController zalExtensionController) throws Exception
   {
-    BootstrapClassLoader bootstrapClassLoader = getBootstrapClassLoader(extensionDirectory);
+    BootstrapClassLoader bootstrapClassLoader = mExtensionLoader.getBootstrapClassLoader(extensionDirectory);
 
     Class<ZalEntrypoint> zalEntrypoint = (Class<ZalEntrypoint>) bootstrapClassLoader.loadClass(sZalEntrypointName);
 
@@ -59,110 +52,6 @@ public class TinyBoot
 
   public ZalEntrypoint createZalEntryPoint(ZalExtensionController controller) throws Exception
   {
-    File extensionRootDirectory = new File(readPath());
-    Version bestVersion = getBestVersion(extensionRootDirectory);
-
-    if (bestVersion == null)
-    {
-      throw new RuntimeException("cannot find a valid extension x.y.z in directory: " + extensionRootDirectory.getAbsolutePath());
-    }
-
-    File chosenExtensionDirectory = new File(extensionRootDirectory, bestVersion.toString());
-
-    return createZalEntryPoint(chosenExtensionDirectory, controller);
-  }
-
-  private BootstrapClassLoader getBootstrapClassLoader(File chosenExtensionDirectory)
-  {
-    File[] nodes = chosenExtensionDirectory.listFiles();
-    List<File> fileList = new LinkedList<File>();
-    if (nodes != null)
-    {
-      for (File node : nodes)
-      {
-        if (node.isFile())
-        {
-          fileList.add(node);
-        }
-      }
-    }
-
-    return createClassLoader(fileList);
-  }
-
-  private BootstrapClassLoader createClassLoader(List<File> fileList)
-  {
-    List<URL> urls = new ArrayList<URL>(fileList.size());
-
-    for (File file : fileList)
-    {
-      try
-      {
-        urls.add(new URL("jar:file:" + file.getAbsolutePath() + "!/"));
-      }
-      catch (MalformedURLException e)
-      {
-        throw new RuntimeException(e);
-      }
-    }
-
-    return new BootstrapClassLoader(
-      urls.toArray(new URL[urls.size()]),
-      this.getClass().getClassLoader(),
-      false
-    );
-  }
-
-  private Version getBestVersion(File realExtensionDirectory)
-  {
-    Version bestVersion = null;
-    File[] files = realExtensionDirectory.listFiles();
-
-    if( files != null )
-    {
-      for( File node : files )
-      {
-        if( node.isDirectory() )
-        {
-          String name = node.getName();
-
-          if( name.matches("[0-9]+\\.[0-9]+\\.[0-9]+") )
-          {
-            Version currentVersion = new Version(name);
-            if( bestVersion == null || bestVersion.lessThan(currentVersion) )
-            {
-              bestVersion = currentVersion;
-            }
-          }
-        }
-      }
-    }
-    return bestVersion;
-  }
-
-  private String readPath() throws IOException
-  {
-    StringBuilder stringBuffer = new StringBuilder(1024);
-
-    InputStream in = new BufferedInputStream(new FileInputStream(mExtensionPathFile));
-    try
-    {
-      while(true)
-      {
-        int read = in.read();
-        if( read == -1 )
-        {
-          break;
-        }
-
-        stringBuffer.append((char)read);
-      }
-    }
-    finally
-    {
-      in.close();
-    }
-
-    return stringBuffer.toString().replaceAll("[\r\n]*", "");
+    return createZalEntryPoint(mExtensionLoader.getBestVersionDirectory(mExtensionPathFile), controller);
   }
 }
