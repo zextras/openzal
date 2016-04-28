4da54f7c0b27d0dbbc353256d589ed1945413f72
Davide Baldo <db@zextras.com>
2016-04-28 14:49:07 +0200

    ZX-3563 added support for zimbra 8.7.0-rc1

--- tests/java/com/zimbra/cs/account/MockProvisioning.java
+++ tests/java/com/zimbra/cs/account/MockProvisioning.java
@@ -65,6 +65,7 @@ public final class MockProvisioning extends com.zimbra.cs.account.Provisioning
   {{
       put("confKey", "configurationValue");
     }}, this);
+
   /* $if MajorZimbraVersion >= 8 $ */
   private final Map<String, ShareLocator> shareLocators = new HashMap<String, ShareLocator>();
 
@@ -465,6 +466,11 @@ $endif $
     return localhost;
   }
 
+  public Server getLocalServerIfDefined()
+  {
+    return null;
+  }
+
   public void modifyAttrs(Entry e, Map<String, ? extends Object> attrs,
                           boolean checkImmutable, boolean allowCallback)
   {
@@ -693,6 +699,11 @@ $endif $
 
   public void deleteDomain(String zimbraId) {
     id2domain.remove(zimbraId);
+  }
+
+  public void deleteDomainAfterRename(String s) throws ServiceException
+  {
+    deleteDomain(s);
   }
 
   public Cos createCos(String name, Map<String, Object> attrs) throws ServiceException {
