3d247d811b64ef23376ace70507d32436622389e
Davide Baldo <db@zextras.com>
2016-07-29 10:17:11 +0200

    ZX-3629 ZxMobile fixed test for 6.0.7 chinese encoding. However _header_ decoding is still broken in zimbra 6.0.7 while it pass on 6.0.8+

--- tests/java/com/zimbra/cs/store/file/StoreManagerSimulator.java
+++ tests/java/com/zimbra/cs/store/file/StoreManagerSimulator.java
@@ -23,6 +23,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.FileOutputStream;
 import java.nio.channels.FileChannel;
 import java.util.UUID;
 
@@ -490,6 +491,31 @@ public final class StoreManagerSimulator extends StoreManager
     public void setFile(com.zextras.lib.vfs.File file)
     {
       mFile = file;
+    }
+
+    @Override
+    public File getFile()
+    {
+      try
+      {
+        File tmpFile = super.getFile();
+        InputStream inputStream = mFile.openInputStreamWrapper();
+        OutputStream outputStream = new FileOutputStream(tmpFile);
+        try
+        {
+          IOUtils.copy(inputStream, outputStream);
+        }
+        finally
+        {
+          outputStream.close();
+          inputStream.close();
+        }
+        return tmpFile;
+      }
+      catch (Exception ex)
+      {
+        throw new RuntimeException(ex);
+      }
     }
 
     public com.zextras.lib.vfs.File getVirtualFile()
