c4e56c612915cbef7b2e23bed4131ab64cc6893f
Cataldo Mazzilli <cataldo@studiostorti.com>
2017-05-11 17:43:56 +0200

    ZX-3935 backup commit, tests and logs missing

--- src/java/org/openzal/zal/Account.java
+++ src/java/org/openzal/zal/Account.java
@@ -366,6 +366,11 @@ public class Account extends Entry
     return Arrays.asList(mAccount.getMailAlias());
   }
 
+  /**
+   * @param  provisioning Provisioning
+   * @return Collection of all addresses of an Account obtained from account.getName() and account.getMailAlias()
+   * properly combined with their relative domainAliases
+   */
   @NotNull
   public Collection<String> getAllAddressesIncludeDomainAliases(Provisioning provisioning)
   {
@@ -402,10 +407,14 @@ public class Account extends Entry
   public Collection<String> getAllAddressesAllowedInFrom(Provisioning provisioning)
   {
     Set<String> addresses = new HashSet<String>();
+
+    //add main address plus mail aliases combined with relative domain aliases
     addresses.addAll(getAllAddressesIncludeDomainAliases(provisioning));
 
+    //add addresses obtained from account multi attribute "zimbraAllowFromAddress"
     addresses.addAll(getAllowFromAddress());
 
+    //add sendAs and sendAsDistList and relative allowed aliases
     /* $if ZimbraVersion >= 8.0.0 $ */
     Map<Right, Set<com.zimbra.cs.account.Entry>>  rights;
     try
@@ -423,6 +432,7 @@ public class Account extends Entry
       {
         if (entry instanceof com.zimbra.cs.account.Account)
         {
+          addresses.add(((com.zimbra.cs.account.Account) entry).getName());
           addresses.addAll(Arrays.asList(((com.zimbra.cs.account.Account) entry).getPrefAllowAddressForDelegatedSender()));
         }
       }
@@ -434,6 +444,7 @@ public class Account extends Entry
       {
         if (entry instanceof com.zimbra.cs.account.DistributionList)
         {
+          addresses.add(((com.zimbra.cs.account.DistributionList) entry).getName());
           addresses.addAll(Arrays.asList((((com.zimbra.cs.account.DistributionList)entry).getPrefAllowAddressForDelegatedSender())));
         }
       }
@@ -443,6 +454,10 @@ public class Account extends Entry
     return addresses;
   }
 
+  /**
+   * @return the address of account's alias obtained from MultiAttribute "zimbraMailAlias" with the addition
+   * of account.getName().
+   */
   @NotNull
   public List<String> getAllAddresses()
   {
