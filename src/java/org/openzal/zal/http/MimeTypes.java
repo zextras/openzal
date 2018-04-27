43d282ba1e6989dc5007e2cc6227a85c3d74e91e
Dino Puller <dp@zextras.com>
2018-04-27 11:50:53 +0200

    Reverted MimeTypes

--- src/java/org/openzal/zal/http/MimeTypes.java
+++
@@ -1,9 +0,0 @@
-package org.openzal.zal.http;
-
-public class MimeTypes
-{
-  public String getCharsetEncoding(String contentType)
-  {
-    return org.eclipse.jetty.http.MimeTypes.inferCharsetFromContentType(contentType);
-  }
-}
