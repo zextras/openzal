116582ac3dacae3631657531685ba173f04eeac4
Luca Schiavoi <luca.schiavoi@zextras.com>
2018-09-07 17:09:12 +0200

    [ZX-4941] Wrapped MultiTermQuery and WildcardQuery from Lucene

--- src/java/org/openzal/zal/lucene/search/TermQuery.java
+++ src/java/org/openzal/zal/lucene/search/TermQuery.java
@@ -5,6 +5,7 @@ import org.openzal.zal.lucene.index.Term;
 public class TermQuery
   extends Query
 {
+
   public TermQuery(Term term)
   {
     super(new org.apache.lucene.search.TermQuery(term.toZimbra(org.apache.lucene.index.Term.class)));
