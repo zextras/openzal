40a4232a17cf2e68bb720db7c3e3d711f707c440
Matteo Battilana <mb@zextras.com>
2018-11-20 09:45:01 +0100

    [ZX-5541] - Added MailboxIndex.getAnalyzer in order to get the correct Analyzer - Added wrap to Analyzer, AttributeSource, TokenStream, ZimbraAnalyzer, CharTermAttribute, BooleanClause, BooleanQuery, PhraseQuery and PrefixQuery

---
+++ src/java/org/openzal/zal/lucene/analysis/AttributeSource.java
@@ -0,0 +1,47 @@
+package org.openzal.zal.lucene.analysis;
+
+import org.jetbrains.annotations.NotNull;
+import org.openzal.zal.lucene.analysis.tokenattributes.CharTermAttribute;
+
+
+public class AttributeSource
+{
+  /* $if ZimbraVersion >= 8.5.0 $ */
+  private final org.apache.lucene.util.AttributeSource mZObject;
+  /* $endif $ */
+
+  public AttributeSource(@NotNull Object zObject)
+  {
+    /* $if ZimbraVersion >= 8.5.0 $ */
+    mZObject = (org.apache.lucene.util.AttributeSource) zObject;
+    /* $endif $ */
+  }
+
+  public CharTermAttribute addCharTermAttribute()  {
+    /* $if ZimbraVersion >= 8.5.0 $ */
+    return new CharTermAttribute(mZObject.addAttribute(org.apache.lucene.analysis.tokenattributes.CharTermAttribute.class));
+    /* $else $
+    throw new UnsupportedOperationException();
+    /* $endif $ */
+  }
+
+  @Override
+  public String toString() {
+    /* $if ZimbraVersion >= 8.5.0 $ */
+    return mZObject.toString();
+    /* $else $
+    throw new UnsupportedOperationException();
+    /* $endif $ */
+  }
+
+  public <T> T toZimbra(
+    @NotNull Class<T> target
+  )
+  {
+    /* $if ZimbraVersion >= 8.5.0 $ */
+    return target.cast(mZObject);
+    /* $else $
+    throw new UnsupportedOperationException();
+    /* $endif $ */
+  }
+}
