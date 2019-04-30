92ec4cf34942fd0b7d01ce7103e3c17cf4672634
Matteo Battilana <matteo.battilana@zextras.com>
2019-04-30 11:37:36 +0200

    [COR-26] - Added RedisClientHelper direct access to key->value

--- src/java/org/openzal/zal/RedisClientHelper.java
+++ src/java/org/openzal/zal/RedisClientHelper.java
@@ -4,6 +4,7 @@ import javax.annotation.*;
 
 import java.util.Set;
 /* $if ZimbraX == 1 $ */
+import org.redisson.api.RBucket;
 import org.redisson.api.RSet;
 import org.redisson.api.RedissonClient;
 /* $endif $ */
@@ -24,18 +25,18 @@ public class RedisClientHelper
   }
 
   @Nullable
-  public String put(@Nonnull String key, String value)
+  public Set<String> putInSet(@Nonnull String key, String value)
   {
     /* $if ZimbraX == 1 $ */
     RSet<String> set = mRedissonClient.getSet(key);
     set.add(value);
-    return value;
+    return set.readAll();
     /* $else $
     throw new UnsupportedOperationException();
     /* $endif $ */
   }
 
-  public Set<String> get(@Nonnull String key)
+  public Set<String> getSet(@Nonnull String key)
   {
     /* $if ZimbraX == 1 $ */
     RSet<String> set = mRedissonClient.getSet(key);
@@ -45,11 +46,44 @@ public class RedisClientHelper
     /* $endif $ */
   }
 
-  public void remove(@Nonnull String key, @Nonnull String value)
+  public void removeFromSet(@Nonnull String key, @Nonnull String value)
   {
     /* $if ZimbraX == 1 $ */
     RSet<String> set = mRedissonClient.getSet(key);
     set.remove(value);
+    /* $else $
+    throw new UnsupportedOperationException();
+    /* $endif $ */
+  }
+
+  @Nullable
+  public String set(@Nonnull String key, String value)
+  {
+    /* $if ZimbraX == 1 $ */
+    RBucket<String> bucket = mRedissonClient.getBucket(key);
+    bucket.set(value);
+    return value;
+    /* $else $
+    throw new UnsupportedOperationException();
+    /* $endif $ */
+  }
+
+  @Nullable
+  public String get(@Nonnull String key)
+  {
+    /* $if ZimbraX == 1 $ */
+    RBucket<String> bucket = mRedissonClient.getBucket(key);
+    return bucket.get();
+    /* $else $
+    throw new UnsupportedOperationException();
+    /* $endif $ */
+  }
+
+  public boolean contains(@Nonnull String key)
+  {
+    /* $if ZimbraX == 1 $ */
+    RBucket<String> bucket = mRedissonClient.getBucket(key);
+    return bucket.isExists();
     /* $else $
     throw new UnsupportedOperationException();
     /* $endif $ */
