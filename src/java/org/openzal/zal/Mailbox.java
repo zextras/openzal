6e8a4e098647ddcdf7bad6f5d2e1e77b1ef3f8ce
Alessandro Piccioli <ap@zextras.com>
2020-01-31 11:36:58 +0100

    Mailbox removed unused imports.

--- src/java/org/openzal/zal/Mailbox.java
+++ src/java/org/openzal/zal/Mailbox.java
@@ -88,8 +88,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 //import com.zimbra.cs.fb.FreeBusy;
 
