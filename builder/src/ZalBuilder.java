696d28ae73b68a146ac48aa3debb6e5b2871233b
Davide Baldo <davide@baldo.me>
2020-10-16 15:28:18 +0000

    IN-151 Fixed missing setup() which was breaking some test deployment

--- builder/src/ZalBuilder.java
+++ builder/src/ZalBuilder.java
@@ -250,6 +250,7 @@ public class ZalBuilder
       }
 
       case "zal-x": {
+        setup(systemReader);
         buildFromZimbraVersion(sZimbraX, systemReader, false);
         return;
       }
@@ -259,6 +260,7 @@ public class ZalBuilder
     }
 
     if( command.startsWith("zal-") ) {
+      setup(systemReader);
       Version zimbraVersion = new Version(command.substring(4));
       buildFromZimbraVersion(new Zimbra(Zimbra.Type.standard, zimbraVersion), systemReader, false);
       return;
