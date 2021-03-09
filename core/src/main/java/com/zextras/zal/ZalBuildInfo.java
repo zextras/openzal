8bfbdfa8d2e833c527274cea534c554f1452e9fd
zextras_giovanni_defacci <giovanni.defacci@zextras.com>
2021-03-09 09:22:40 +0100

    maven spike

---
+++ core/src/main/java/com/zextras/zal/ZalBuildInfo.java
@@ -0,0 +1,7 @@
+package com.zextras.zal;
+
+public class ZalBuildInfo
+{
+  public final static String COMMIT="0";
+  public final static String Version="0.0.1-SNAPSHOT";
+}
---
+++ core/src/main/java/org/openzal/zal/ZalBuildInfo.java
@@ -0,0 +1,6 @@
+package org.openzal.zal;
+
+public class ZalBuildInfo {
+  public final static String COMMIT="0";
+  public final static String VERSION="1";
+}
