3f4459428f1d7c052f4004cbcb2a48f48cea3648
Marco Zanella <marco.zanella@zextras.com>
2022-03-15 17:02:34 +0100

    Add unset mail quota method to Account

--- src/java/org/openzal/zal/Account.java
+++ src/java/org/openzal/zal/Account.java
@@ -1313,5 +1313,16 @@ public class Account extends Entry
   public boolean mustChangePassword() {
     return mAccount.isPasswordMustChange();
   }
+
+  public void unsetMailQuota() {
+    try
+    {
+      this.mAccount.unsetMailQuota();
+    }
+    catch (ServiceException e)
+    {
+      throw ExceptionWrapper.wrap(e);
+    }
+  }
 }
 
