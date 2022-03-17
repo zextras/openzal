2f8c4ed89dcb610f304f20a7231588c94870c357
Marco Zanella <marco.zanella@zextras.com>
2022-03-17 11:04:00 +0100

    Add invalidate all tokens method to Account

--- src/java/org/openzal/zal/Account.java
+++ src/java/org/openzal/zal/Account.java
@@ -1331,5 +1331,19 @@ public class Account extends Entry
       throw ExceptionWrapper.wrap(e);
     }
   }
+
+  public boolean invalidateAllTokens() {
+    try {
+      if (!mAccount.getProvisioning().getConfig().isAuthTokenValidityValueEnabled()) {
+        return false;
+      }
+
+      int validityValue = mAccount.getAuthTokenValidityValue();
+      mAccount.setAuthTokenValidityValue(validityValue == Integer.MAX_VALUE ? 0 : ++validityValue);
+      return true;
+    } catch (ServiceException e) {
+      throw ExceptionWrapper.wrap(e);
+    }
+  }
 }
 
