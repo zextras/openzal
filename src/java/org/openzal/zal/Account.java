da60fe05caf28bf41febcaaeb19f51166e00f8e8
zextras_giovanni_defacci <giovanni.defacci@zextras.com>
2022-11-03 16:55:01 +0100

    Expand star import

--- src/java/org/openzal/zal/Account.java
+++ src/java/org/openzal/zal/Account.java
@@ -47,7 +47,16 @@ import org.openzal.zal.extension.ConfigZimletStatus;
 import org.openzal.zal.soap.SoapTransport;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Account extends Entry
 {
