d3b73ec7c6906ca22e7b2a3673d263c38a1f7626
Giovanni De Facci <giovanni.defacci@zextras.com>
2023-07-06 16:11:09 +0200

    GetAllServer return only reachable servers

--- src/java/org/openzal/zal/ProvisioningImp.java
+++ src/java/org/openzal/zal/ProvisioningImp.java
@@ -32,8 +32,10 @@ import com.zimbra.cs.mailbox.Folder;
 import com.zimbra.cs.mailbox.MailItem;
 import com.zimbra.cs.mailbox.MailServiceException;
 import com.zimbra.cs.mailbox.Mailbox;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.Socket;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -61,6 +63,7 @@ import com.zimbra.cs.ldap.ZSearchControls;
 import com.zimbra.cs.ldap.ZSearchScope;
 import com.zimbra.cs.ldap.unboundid.UBIDLdapContext;
 import com.zimbra.cs.util.ProxyPurgeUtil;
+import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
 import com.zimbra.soap.ZimbraSoapContext;
@@ -1218,13 +1221,35 @@ public class ProvisioningImp implements Provisioning
     }
   }
 
+  public static final int HEALTH_CHECK_PORT = 8743;
+  private static boolean isReachable(com.zimbra.cs.account.Server server) {
+    Socket socket = null;
+    try {
+      String ipAddress = server.getIPAddress();
+      socket = new Socket(ipAddress, HEALTH_CHECK_PORT);
+      return true;
+    } catch (Exception e) {
+      ZimbraLog.extensions.warn(String.format("Address %s:%s is not reachable", server.getHostname(), HEALTH_CHECK_PORT));
+      return false;
+    } finally {
+      try {
+        if (socket != null) socket.close();
+      } catch (IOException e) {
+      }
+    }
+  }
+
   @Override
   public List<Server> getAllServers()
     throws ZimbraException
   {
     try
     {
-      return ZimbraListWrapper.wrapServers(mProvisioning.getAllServers());
+      List<com.zimbra.cs.account.Server> allServers = mProvisioning.getAllServers()
+          .stream()
+          .filter(ProvisioningImp::isReachable)
+          .collect(Collectors.toList());
+      return ZimbraListWrapper.wrapServers(allServers);
     }
     catch (com.zimbra.common.service.ServiceException e)
     {
@@ -1238,7 +1263,11 @@ public class ProvisioningImp implements Provisioning
   {
     try
     {
-      return ZimbraListWrapper.wrapServers(mProvisioning.getAllServers(service));
+      List<com.zimbra.cs.account.Server> allServers = mProvisioning.getAllServers(service)
+          .stream()
+          .filter(ProvisioningImp::isReachable)
+          .collect(Collectors.toList());
+      return ZimbraListWrapper.wrapServers(allServers);
     }
     catch (com.zimbra.common.service.ServiceException e)
     {
